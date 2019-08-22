9ada99bc8678caf096aa6bbb614cda30878c3e13
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-08-22 18:21:26 +0100

    Checked if user exist

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
 const { check, validationResult } = require('express-validator');
 
+const User = require('../../models/User');
+
+// @route   POST api/users
+// @desc    Register user
+// @access  Public
 router.post(
   '/',
   [
@@ -14,12 +20,38 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+
+      let user = User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exist' }] });
+      }
+
+      // Get user gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      // Encrypt password
+
+      // Return jsonwebtoken
+      res.send('User route');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
