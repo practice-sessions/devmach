8b823332825ffe1e22795a8b54f2d837e4cfadae
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-08-22 18:39:32 +0100

    Used bcrypt to hash users password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -44,7 +45,17 @@ router.post(
         d: 'mm'
       });
 
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
       // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
 
       // Return jsonwebtoken
       res.send('User route');
