6197d8522660094c80961da8ca45787d4b253304
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-08-26 15:22:48 +0100

    Added config and package-lock.json files to gitignore file

--- config/db.js
+++ config/db.js
@@ -11,7 +11,7 @@ const connectDB = async () => {
 
     console.log('MongoDB Connected...');
   } catch (err) {
-    console.log(err.message);
+    console.console.error(err.message);
     // Exit process with failure
     process.exit(1);
   }
