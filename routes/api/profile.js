d5f3f0eefa84e557e8eff5ea06b40b4faf2c204d
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-10-18 10:44:04 +0100

    Saved Updated profile to database

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -97,6 +97,12 @@ router.post(
 
         return res.json(profile);
       }
+
+      //Create
+      profile = new Profile(profilefields);
+
+      await Profile.save();
+      res.json(profile);
     } catch (err) {
       console.error(err.mesage);
       res.status(500).send('Server Error');
