0a32c79dc14a13f692d1542e6a38a1de7a13446e
Mekarosi <mekarosi_assa@yahoo.com>
2020-01-27 13:40:39 +0100

    Create route to get all post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const config = require('config');
 const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
 
@@ -47,5 +46,19 @@ router.post(
     }
   }
 );
+
+// @route  GET api/posts
+// @desc   Get all post
+// @access Private
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
