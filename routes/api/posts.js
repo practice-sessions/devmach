25ce0e06167c8a06de98c746909ed9f27780bf8f
Mekarosi <mekarosi_assa@yahoo.com>
2020-01-28 10:43:30 +0100

    Create route to unlike post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -112,7 +112,7 @@ router.delete('/:id', auth, async (req, res) => {
 });
 
 // @route  PUT api/posts/like/:id
-// @desc   like a post
+// @desc   Like a post
 // @access Private
 
 router.put('/like/:id', auth, async (req, res) => {
@@ -126,6 +126,38 @@ router.put('/like/:id', auth, async (req, res) => {
       return res.status(400).json({ msg: 'Post already liked by you' });
     }
     post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  PUT api/posts/unlike/:id
+// @desc   unlike a post
+// @access Private
+
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has already been liked
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).json({ msg: 'Post has not yet been liked' });
+    }
+
+    // Get remove index
+    const removeIndex = post.likes.map(like =>
+      like.user.toString().indexOf(req.user.id)
+    );
+
+    post.likes.splice(removeIndex, 1);
 
     await post.save();
 
