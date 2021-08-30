6c2c8b9d7b8d567be7f3283f21311ae6af382626
Midland digital <mekarosi@midlanddigitalsolutions.com>
2021-08-30 11:01:20 +0100

    Authenticate user

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,8 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types'
+import { 
+    REGISTER_SUCCESS, REGISTER_FAIL, 
+    USER_LOADED, 
+    AUTH_ERROR 
+} from '../actions/types'
 
 const initialState = {
     token: localStorage.getItem('token'),
@@ -12,6 +16,13 @@ const initialState = {
 const { type, payload } = action
 
 switch(type) {
+    case USER_LOADED:
+        return{
+            ...state,
+            isAuthenticated: true,
+            loading: false,
+            user: payload
+        }
     case REGISTER_SUCCESS:
         localStorage.setItem('token', payload.token)
         return {
@@ -21,6 +32,7 @@ switch(type) {
             loading: false
         }
     case REGISTER_FAIL:
+    case AUTH_ERROR:    
         localStorage.removeItem('token')
         return {
             ...state,
