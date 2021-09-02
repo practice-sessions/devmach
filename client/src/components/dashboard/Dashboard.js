016e5063681fedf144d5c6656e6093915a05a46b
Midland digital <mekarosi@midlanddigitalsolutions.com>
2021-09-02 21:40:34 +0100

    Create edit profile component and action

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
+import DashboardActions from './DashboardActions'
 import { getCurrentProfile } from '../../actions/profile'
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
@@ -19,7 +20,9 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
             <p className='lead'>
                 <i className="fas fa-user"></i> Welcome { user && user.name }
             </p>
-           {profile !== null ? <Fragment>has</Fragment> : 
+           {profile !== null ? <Fragment>
+               <DashboardActions />
+           </Fragment> : 
            
            <Fragment>
                <p>You have not yet setup a profile, please add some info</p>
@@ -28,8 +31,11 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
                </Link>
                
             </Fragment>}
-
-
+            <br/>
+          
+            <Link to='/create-profile' className='btn btn-primary my-1'>
+                   Create Profile
+               </Link>
         </Fragment>
         )
             
