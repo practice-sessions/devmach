0d182522e840d8d5fe5b498734febc6df1b630e5
Midland digital <mekarosi@midlanddigitalsolutions.com>
2021-09-02 10:34:26 +0100

    Add user dashboard features

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -3,7 +3,8 @@ import { setAlert } from './alert'
 import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR,
 LOGIN_SUCCESS,
 LOGIN_FAIL,
-LOGOUT
+LOGOUT,
+CLEAR_PROFILE
 } from './types'
 import setAuthToken from '../utils/setAuthToken'
 
@@ -106,5 +107,6 @@ export const login = ( email, password) => async dispatch => {
 
 // Logout & Clear Profile
 export const logout = () => dispatch => {
-   dispatch({ type: LOGOUT }) 
+   dispatch({ type: CLEAR_PROFILE })
+   dispatch({ type: LOGOUT })   
 }
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR } from "../actions/types"
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from "../actions/types"
 
 const initialState = {
     profile: null,
@@ -23,7 +23,13 @@ switch(type){
             profile: payload,
             loading: false
         }
-        
+    case CLEAR_PROFILE:   
+        return {
+            ...state,
+            profile: null,
+            repos: [],
+            loading: false
+        }  
     default: 
         return state
 
