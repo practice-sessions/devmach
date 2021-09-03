f4189e851b224cb887a91a7545b5b975d65cf094
Midland digital <mekarosi@midlanddigitalsolutions.com>
2021-09-03 21:52:11 +0100

    Create Add experience and Add education components and actions

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from "../actions/types"
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from "../actions/types"
 
 const initialState = {
     profile: null,
@@ -12,6 +12,7 @@ const initialState = {
     const { type, payload } = action
 switch(type){
     case GET_PROFILE:
+    case UPDATE_PROFILE: 
         return {
             ...state,
             profile:payload,
