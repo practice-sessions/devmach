7377898a4291ce0cad6301bc5786621dc1600069
Midland digital <mekarosi@midlanddigitalsolutions.com>
2021-09-05 14:35:46 +0100

    Create actions to get all profile and repos

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -5,6 +5,8 @@ import {
     ACCOUNT_DELETED,
     CLEAR_PROFILE,
     GET_PROFILE,
+    GET_PROFILES,
+    GET_REPOS,
     PROFILE_ERROR,
     UPDATE_PROFILE
 } from './types'
@@ -17,6 +19,62 @@ export const getCurrentProfile = () => async dispatch => {
 
         dispatch({
             type: GET_PROFILE,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+// Get all profiles
+export const getProfiles = () => async dispatch => {
+    dispatch({ type: CLEAR_PROFILE })
+    try {
+        const res = await axios.get('/api/profile')
+
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+
+// Get profile by ID
+export const getProfileById = userId => async dispatch => {
+   
+    try {
+        const res = await axios.get(`/api/profile/user/${userId}`)
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+
+// Get Github repos
+export const getGithubRepos = username => async dispatch => {
+   
+    try {
+        const res = await axios.get(`/api/profile/github/${username}`)
+
+        dispatch({
+            type: GET_REPOS,
             payload: res.data
         })
     } catch (err) {
