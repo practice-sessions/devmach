db40477467e8311f8363b72e45f71c93d9b8ad03
Midland digital <mekarosi@midlanddigitalsolutions.com>
2021-09-06 21:24:45 +0100

    Add more items to users profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
+import ProfileTop from './ProfileTop'
+import ProfileAbout from './ProfileAbout'
 import { getProfileById } from '../../actions/profile'
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
 
     useEffect(() => {
         getProfileById(match.params.id)
-    }, [getProfileById])
+    }, [getProfileById, match.params.id])
 
 
     return (
@@ -21,6 +23,10 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
               {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='/edit-profile' className='btn btn-dark'>
                   Edit Profile
               </Link>)}
+              <div className="profile-grid my-1">
+                <ProfileTop profile={profile} /> 
+                <ProfileAbout profile={profile} /> 
+              </div>
           </Fragment> }
         </Fragment>
     )
